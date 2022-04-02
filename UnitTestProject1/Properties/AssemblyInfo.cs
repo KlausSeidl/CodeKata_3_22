60ab5f23039b8d10186543a6d46fba0d7acced9d
Klaus Seidl <kseidl@recom.eu>
2022-04-02 19:06:15 +0200

    Added MSTestProject

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9c3ec4ca-6894-4ec4-b5f6-324d02daf160")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Calculator;
+using FluentAssertions;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class FieldAxiomTests
+    {
+        [TestMethod]
+        public void Add_should_respect_neutral_element_off_addition()
+        {
+            var result = SimpleCalculator.Add(1, 0);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Diff_should_respect_inverse_element_off_addition()
+        {
+            var result = SimpleCalculator.Diff(1, 1);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Mult_should_respect_neutral_element_off_multiplication()
+        {
+            var result = SimpleCalculator.Mult(5, 1);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Div_should_respect_inverse_element_off_multiplication()
+        {
+            var result = SimpleCalculator.Div(5, 5);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Div_by_zero_should_throw_exception()
+        {
+            Action action = () => SimpleCalculator.Div(5, 0);
+            Assert.ThrowsException<DivideByZeroException>(action);
+        }
+    }
+}
